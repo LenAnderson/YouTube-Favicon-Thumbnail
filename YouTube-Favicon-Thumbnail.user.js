0359f782bd08295a78bbcf1d928f60b6d2269204
Len <LenAnderson@users.noreply.github.com>
2021-10-19 13:12:53 +0200

    Create YouTube-Favicon-Thumbnail.user.js

---
+++ YouTube-Favicon-Thumbnail.user.js
@@ -0,0 +1,39 @@
+// ==UserScript==
+// @name         YouTube - Favicon Thumbnail
+// @namespace    https://github.com/LenAnderson
+// @downloadURL  https://github.com/LenAnderson/YouTube-Favicon-Thumbnail/raw/master/YouTube-Favicon-Thumbnail.user.js
+// @version      0.1
+// @description  Replace YouTube favicon with video thumbnail
+// @author       LenAnderson
+// @match        https://www.youtube.com/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=youtube.com
+// @grant        none
+// ==/UserScript==
+
+(async function() {
+    'use strict';
+
+    const log = (...msgs)=>console.log.call(console.log, '[YT-FT]', ...msgs);
+
+    const $ = (root,query)=>(query?root:document).querySelector(query?query:root);
+    const $$ = (root,query)=>Array.from((query?root:document).querySelectorAll(query?query:root));
+
+    const wait = async(millis)=>(new Promise(resolve=>setTimeout(resolve,millis)));
+
+
+	const updateFavicon = async()=>{
+		try {
+			const idParam = location.search.substring(1).split('&').find(it=>it.substring(0,2)=='v=');
+			if (idParam) {
+				const id = idParam.substring(2);
+				const thumbUrl = `https://i.ytimg.com/vi/${id}/hqdefault.jpg`;
+				Array.from(document.body.parentElement.querySelectorAll('[rel*="icon"]')).forEach(icon=>icon.href = thumbUrl);
+			}
+		} catch (ex) {
+			log(ex);
+		}
+	};
+
+	const mo = new MutationObserver(updateFavicon);
+	mo.observe(document.body, {attributes: true, childList: true, subtree: true });
+})();
